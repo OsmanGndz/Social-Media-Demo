024e408d92c9697c9a594ff02f14c4b3288bef8d
Osman Gündüz <4242osman424@gmail.com>
2025-09-06 22:38:03 +0300

    Codebase is completed.
    - React vite
    - Typescript
    - Tailwindcss
    - Axios
    - React-icons

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+function App() {
+  return <div className="w-full items-center justify-center text-violet-600 font-poppins">
+    <h1 className="text-violet-600 text-xl">asdasd</h1>
+  </div>;
+}
+
+export default App;
